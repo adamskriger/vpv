930f9c8f27b6c37d89fb5b5c8ae969bb173b858a
github username <github email address>
2016-04-14 09:09:17 -0400

    Scroll feature now functional

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 
   $(window).bind('scroll', function() {
-	   var navHeight = $( window ).height() - 51;
+	   var navHeight = $( window ).height() - 50.1563;
 			 if ($(window).scrollTop() > navHeight) {
 				 $('nav').addClass('fixed');
 			 }
@@ -11,5 +11,21 @@ $(document).ready(function(){
 		});
 
 
+    $('.scroll-link').on('click', function(event){
+    		event.preventDefault();
+    		var sectionID = $(this).attr("data-id");
+    		scrollToID('#' + sectionID, 750);
+    	});
+
+      function scrollToID(id, speed){
+	var offSet = 50;
+	var targetOffset = $(id).offset().top - offSet;
+	var mainNav = $('#main-nav');
+	$('html,body').animate({scrollTop:targetOffset}, speed);
+	if (mainNav.hasClass("open")) {
+		mainNav.css("height", "1px").removeClass("in").addClass("collapse");
+		mainNav.removeClass("open");
+	}
+}
 
  });
